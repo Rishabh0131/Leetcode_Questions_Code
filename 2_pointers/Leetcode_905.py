185bb9333a507320058d266f0cd6ec84f58a4db5
Rishabh0131 <arishabh1997@gmail.com>
2026-02-12 17:17:17 +0530

    LeetCode-905: Sort Array By Parity

---
+++ 2_pointers/Leetcode_905.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            if nums[left] % 2 != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
+            else:
+                left += 1
+
+        return nums
