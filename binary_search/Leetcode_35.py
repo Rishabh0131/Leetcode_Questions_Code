2eed2be600e4a0a1109da40851f41d25043c3a42
Rishabh0131 <arishabh1997@gmail.com>
2026-02-19 21:41:03 +0530

    LeetCode-35: Search Insert Position

---
+++ binary_search/Leetcode_35.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        N = len(nums)
+        L = 0
+
+        R = N - 1
+
+        while L <= R:
+            M = L + ((R - L) // 2)
+
+            if nums[M] == target:
+                return M
+            elif target < nums[M]:
+                R = M - 1
+            else:
+                L = M + 1
+
+        return L + 1
