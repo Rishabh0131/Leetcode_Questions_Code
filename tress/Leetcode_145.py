4d8ac0e1bed0b42ba63f47e2ae317a1c954d09fb
Rishabh0131 <arishabh1997@gmail.com>
2026-02-26 00:47:54 +0530

    LeetCode-145: Binary Tree Postorder Traversal

---
+++ tress/Leetcode_145.py
@@ -0,0 +1,32 @@
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        visited = [False]
+        res = []
+
+        while stack:
+            curr, vist = stack.pop(), visited.pop()
+
+            if curr:
+                if vist:
+                    res.append(curr.val)
+                else:
+                    stack.append(curr)
+                    visited.append(True)
+                    stack.append(curr.right)
+                    visited.append(False)
+                    stack.append(curr.left)
+                    visited.append(False)
+
+        return res
