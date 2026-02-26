756cb2dbeca1d8d027fd6d890237c7153a86b82a
Rishabh0131 <arishabh1997@gmail.com>
2026-02-26 16:11:54 +0530

    LeetCode-590: N-ary Tree Postorder Traversal

---
+++ tress/Leetcode_590.py
@@ -0,0 +1,34 @@
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class Node:
+    def __init__(
+        self, val: Optional[int] = None, children: Optional[List["Node"]] = None
+    ):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def postorder(self, root: "Node") -> List[int]:
+
+        if root is None:
+            return []
+
+        res = []
+
+        stack = [(root, False)]
+
+        while stack:
+            node, visited = stack.pop()
+
+            if visited:
+                res.append(node.val)
+            else:
+                stack.append((node, True))
+
+                for child in node.children[::-1]:  # type: ignore
+                    stack.append((child, False))
+
+        return res
